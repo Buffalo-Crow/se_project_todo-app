873d3e5c12bf3e52bc025e81e104c8dddca2c629
crowwalk <crowwalk@icloud.com>
2024-11-16 11:59:32 -0500

    add Section Class and functionality

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,30 @@ import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
 import FormValidator from "../components/FormValidator.js";
+import Section from "../components/Section.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
 const addTodoForm = document.forms["add-todo-form"];
 const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
-//const todoTemplate = document.querySelector("#todo-template"); - remove
 const todosList = document.querySelector(".todos__list");
+
+const generateTodo = (data) => {
+  const todo = new Todo(data, "#todo-template");
+  const todoElement = todo.getView();
+  return todoElement;
+};
+
+const todoSection = new Section({
+  items: initialTodos,
+  renderer: (item) => {
+    const todoSectionEl = generateTodo(item);
+    todoSection.addItem(todoSectionEl);
+  },
+  containerSelector: ".todos__list",
+});
+
+todoSection.renderItems();
 
 const openModal = (modal) => {
   modal.classList.add("popup_visible");
@@ -16,13 +33,6 @@ const openModal = (modal) => {
 
 const closeModal = (modal) => {
   modal.classList.remove("popup_visible");
-};
-
-// The logic in this function should all be handled in the Todo class.
-const generateTodo = (data) => {
-  const todo = new Todo(data, "#todo-template");
-  const todoElement = todo.getView();
-  return todoElement;
 };
 
 addTodoButton.addEventListener("click", () => {
@@ -49,13 +59,13 @@ addTodoForm.addEventListener("submit", (evt) => {
 
   const id = uuidv4();
   const values = { name, date, id };
-  renderTodo(item);
+  renderTodo(todoSection);
   closeModal(addTodoPopup);
 });
 
-initialTodos.forEach((item) => {
-  renderTodo(item);
-});
+//initialTodos.forEach((item) => {
+//  renderTodo(item);
+//});
 
 const newTodoValidator = new FormValidator(validationConfig, addTodoForm);
 newTodoValidator.enableValidation();
